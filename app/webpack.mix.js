d5c4546be7240239d438cb925bd6a62374dbb969
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-03 22:17:03 +0200

    Toten Code gelöscht

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -17,14 +17,6 @@ mix.js('resources/js/app.js', 'public/js')
 
 
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-// module.exports = {
-//     plugins: [
-//         new MonacoWebpackPlugin({
-//             // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-//             languages: ['javascript','c','cpp','java','python']
-//         })
-//     ]
-// }
 mix.webpackConfig(webpack => {
     return {
         plugins: [
