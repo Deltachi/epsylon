e7eca6151ddfa6a759d6c66ec51b297eaaab328a
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-03 19:50:49 +0200

    jquery-ui import

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -8,6 +8,9 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import 'jquery-ui/ui/widgets/draggable.js';
+import 'jquery-ui/ui/widgets/droppable.js';
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
