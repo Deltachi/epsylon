2881e207dfd5f7225ee85e609ef8148e2cf5d22b
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-22 20:00:57 +0200

    Testdaten gefüttert

--- app/database/migrations/2020_06_22_124120_create_tasks_table.php
+++ app/database/migrations/2020_06_22_124120_create_tasks_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateTasksTable extends Migration
@@ -14,7 +15,7 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->id()->autoIncrement();
             $table->integer('type')->comment("Typ der Aufgabe, für das richtige parsen der JSON Daten.");
             $table->char('title', 100)->comment("Titel der Aufgabe");
             $table->longText('description')->comment("Aufgabenbeschreibung");
@@ -23,6 +24,51 @@ class CreateTasksTable extends Migration
             $table->json('solution')->comment("Erwartungshorizont zur Aufgabe im JSON-Format");
             $table->timestamps();
         });
+
+        DB::table('tasks')->insert(
+          [
+              'type' => 0,
+              'title' => "Das hier ist eine Aufgabe mit vielen Fragen",
+              'description' => "<p>In dieser Aufgabe sollst du Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ut aliquip ex ea commodo consequat. Duis aute irure dolor anim id est laborum. machen.</p>",
+              'hint' => "Hier steht ein Hinweis zur Aufgabe",
+              'data' => json_encode(array("Subtasks"=> array(
+                  array(
+                      "id" => 0,
+                      "title" => "Hier ist die Frage 1",
+                      "hint" => "Hier ist der 1. Hinweis"
+                  ),
+                  array(
+                      "id" => 1,
+                      "title" => "Hier ist die Frage 2",
+                      "hint" => "Hier ist der 2. Hinweis"
+                  ),
+                  array(
+                      "id" => 2,
+                      "title" => "Hier ist die Frage 3",
+                      "hint" => "Hier ist der 3. Hinweis"
+                  )
+                )
+              )),
+              'solution' => json_encode(array("Subtasks"=> array(
+                  array(
+                      "id" => 0,
+                      "solution" => "Lösung zur Aufgabe 1",
+                      "points" => 2
+                  ),
+                  array(
+                      "id" => 1,
+                      "solution" => "Lösung zur Aufgabe 2",
+                      "points" => 2
+                  ),
+                  array(
+                      "id" => 2,
+                      "solution" => "Lösung zur Aufgabe 3",
+                      "points" => 3
+                  )
+              )
+              )),
+          ]
+        );
     }
 
     /**
