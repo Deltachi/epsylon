9995fc5fc4d3ca2a258f485ef1add71f23bc4ff8
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-23 03:02:50 +0200

    Refactor: For-Schleife, die 9 Links generiert. Aktives Element bekommt Klasse="active"

--- app/resources/views/task.blade.php
+++ app/resources/views/task.blade.php
@@ -4,17 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-3">
-            <ul class="list-group">
-                <li class="list-group-item"><a href="/task/1">Aufgabenstellung 1</a></li>
-                <li class="list-group-item"><a href="/task/2">Aufgabenstellung 2</a></li>
-                <li class="list-group-item"><a href="/task/3">Aufgabenstellung 3</a></li>
-                <li class="list-group-item"><a href="/task/4">Aufgabenstellung 4</a></li>
-                <li class="list-group-item"><a href="/task/5">Aufgabenstellung 5</a></li>
-                <li class="list-group-item"><a href="/task/6">Aufgabenstellung 6</a></li>
-                <li class="list-group-item"><a href="/task/7">Aufgabenstellung 7</a></li>
-                <li class="list-group-item"><a href="/task/8">Aufgabenstellung 8</a></li>
-                <li class="list-group-item"><a href="/task/9">Aufgabenstellung 9</a></li>
-            </ul>
+            <div class="list-group">
+                @for($i = 1; $i <= 9; $i++)
+                    <a class="list-group-item list-group-item-action {{$task_id == $i ? 'active':''}}" href="/task/{{$i}}">Aufgabenstellung {{$i}}</a>
+                @endfor
+            </div>
         </div>
         <div class="col-md-9">
             <{{$component}} :data-task="'{{$task_data}}'"></{{$component}}>
