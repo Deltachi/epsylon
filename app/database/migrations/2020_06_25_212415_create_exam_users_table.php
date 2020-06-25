bc6adb786cdc26c977af07042682228bb0cbbdcb
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-25 23:30:46 +0200

    ExamUsers-Tabelle speichert welche Studenten welche Klausur schreiben. ExamTasks-Tabelle speichert, welche Aufgaben in welcher Klausur vorkommen.

---
+++ app/database/migrations/2020_06_25_212415_create_exam_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_users', function (Blueprint $table) {
+            $table->foreignId('exam_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->primary(array('exam_id','user_id'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_users');
+    }
+}
