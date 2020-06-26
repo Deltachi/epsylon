8d386cf2856cd1b0ba4ffd70fe285629d5311a41
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-26 18:48:14 +0200

    Klausur-Aufgaben-Tabelle: Seeder hinzugefügt und aktualisiert

--- app/database/seeds/ExamTasksTableSeeder.php
+++ app/database/seeds/ExamTasksTableSeeder.php
@@ -12,10 +12,20 @@ class ExamTasksTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('exam_users')->insert([
+        DB::table('exam_tasks')->insert([
             'exam_id' => 1,
             'task_id' => 1,
+            'points' => 2.0,
+        ]);
+        DB::table('exam_tasks')->insert([
+            'exam_id' => 1,
+            'task_id' => 2,
             'points' => 10.0,
+        ]);
+        DB::table('exam_tasks')->insert([
+            'exam_id' => 1,
+            'task_id' => 3,
+            'points' => 4.0,
         ]);
     }
 }
