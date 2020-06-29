4794617e748edbf4f2c1ac6f08a04b3cbd2367f1
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-30 00:48:19 +0200

    Refactor: Benutze Exam Data direkt anstatt ein Task-Array zu bauen

--- app/resources/views/exam/show.blade.php
+++ app/resources/views/exam/show.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
 <div class="container">
-    <exam :data-tasks="'{{json_encode($tasks, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG)}}'"
-          :data-exam="'{{json_encode($exam, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG)}}'"
+    <exam :data-exam="'{{json_encode($exam, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG)}}'"
           :data-user="'{{json_encode($user, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_TAG)}}'"
     ></exam>
 </div>
