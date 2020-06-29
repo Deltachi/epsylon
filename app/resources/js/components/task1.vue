0a0ce7cf2e71d51157b6ee8410f14e26175ef0d4
Dominik <11911481+Deltachi@users.noreply.github.com>
2020-06-29 17:45:41 +0200

    Refactor: Stripdown aller Logik. Laden, Speichern und Handlers sind jetzt nur noch in der Task-Komponente.

--- app/resources/js/components/task1.vue
+++ app/resources/js/components/task1.vue
@@ -5,50 +5,33 @@
             <div class="card-body">
                 <input v-model="answer" name="answer" type="text" class="form-control" placeholder="Antwort..." >
             </div>
-            <task-footer></task-footer>
+            <task-footer @save="triggerSave" @reset="triggerReset"></task-footer>
         </form>
-        <task-server-message v-if="server_message" v-bind:message="server_message" v-bind:message_type="server_message_type" :animation_handle="server_message_handle"></task-server-message>
     </div>
-    <task-loading-error v-else></task-loading-error>
 </template>
 
 <script>
     import TaskHeader from "./TaskHeader";
     import TaskFooter from "./TaskFooter";
-    import TaskServerMessage from "./TaskServerMessage";
-    import TaskLoadingError from "./TaskLoadingError";
 
     export default {
         name: "task1",
         props: [
             'dataTask',
             'dataAnswer',
-            'dataUserId',
-            'dataExamId',
-            'dataTaskId',
         ],
         components: {
             TaskHeader,
             TaskFooter,
-            TaskServerMessage,
-            TaskLoadingError,
         },
         created() {
-            //Methodenaufrufe
-            this.loadTask();
-            //Variablenzuweisungen
+            //Initialisierungen
             if(this.dataTask && this.dataTask !== "null"){
-                this.task = JSON.parse(this.dataTask);
+                this.convertTaskData(this.dataTask);
                 this.ready = true;
             }
-            if(this.dataUserId && this.dataUserId !== "null"){
-                this.user_id = this.dataUserId;
-            }
-            if(this.dataExamId && this.dataExamId !== "null"){
-                this.exam_id = this.dataExamId;
-            }
-            if(this.dataTaskId && this.dataTaskId !== "null"){
-                this.task_id = this.dataTaskId;
+            if(this.dataAnswer && this.dataAnswer !== "null"){
+                this.convertAnswerData(this.dataAnswer);
             }
         },
         data(){
@@ -72,130 +55,28 @@
             }
         },
         methods: {
-            loadTask(){
-                //this.localLoad();
-
-                //Database connection
-                let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-                let url = '/answer/'+this.user_id+"/"+this.exam_id+"/"+this.task_id;
-                fetch(url, {
-                    method: 'GET',
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json, text-plain, */*",
-                        "X-Requested-With": "XMLHttpRequest",
-                        "X-CSRF-TOKEN": token
-                    },
-                    credentials: "same-origin",
-                })
-                .then(response => response.json())
-                .then(data => this.serverData(data))
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+            convertTaskData(data){
+                this.task = data;
             },
-            submitTask(){
-                //this.localSave();
-
-                //Database connection
-                let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-                let url = '/answer';
-                fetch(url, {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json, text-plain, */*",
-                        "X-Requested-With": "XMLHttpRequest",
-                        "X-CSRF-TOKEN": token
-                    },
-                    credentials: "same-origin",
-                    body: JSON.stringify({
-                        user: this.user_id,
-                        exam: this.exam_id,
-                        task: this.task_id,
-                        data: this.answer
-                    }),
-                })
-                .then(response => response.json())
-                .then(data => this.serverMessage(data))
-                .catch((error) => {
-                    console.error('Error:', error);
-                });
+            getAnswerConversion(data){
+                return data;
             },
-            localSave(){
-                localStorage.setItem("task_"+this.type,JSON.stringify(this.answer));
+            convertAnswerData(data){
+                console.log("convertAnswerData() ",data);
+                this.answer = this.getAnswerConversion(data);
             },
-            localLoad(){
-                if(localStorage.getItem("task_"+this.type)){
-                    this.answer = JSON.parse(localStorage.getItem("task_"+this.type));
-                }
+            triggerSave(){
+                this.$emit('save');
             },
-            localDelete(){
-                if(localStorage.getItem("task_"+this.type)){
-                    localStorage.removeItem("task_"+this.type);
-                }
-            },
-            resetTask(affirmation = false){
-                if(affirmation || confirm("Möchten Sie die Bearbeitung Ihrer Aufgabe zurücksetzen?")){
-                    //this.localDelete();
-                    this.answer = null
-
-                    //Database connection
-                    let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-                    let url = '/answer';
-                    fetch(url, {
-                        method: 'DELETE',
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Accept": "application/json, text-plain, */*",
-                            "X-Requested-With": "XMLHttpRequest",
-                            "X-CSRF-TOKEN": token
-                        },
-                        credentials: "same-origin",
-                        body: JSON.stringify({
-                            user: this.user_id,
-                            exam: this.exam_id,
-                            task: this.task_id,
-                        }),
-                    })
-                        .then(response => response.json())
-                        .then(data => this.serverMessage(data))
-                        .catch((error) => {
-                            console.error('Error:', error);
-                        });
-                }
-            },
-            keyEvent(event) {
-                if (event.ctrlKey || event.metaKey) {
-                    switch (String.fromCharCode(event.which).toLowerCase()) {
-                        case 's':
-                            event.preventDefault();
-                            this.submitTask();
-                            break;
-                    }
-                }
-            },
-            serverMessage(response){
-                console.log(response.message);
-                this.server_message = response.message;
-                this.server_message_type = response.message_type;
-
-                this.server_message_handle.$emit('animate');
-            },
-            serverData(response){
-                if(response.success){
-                    console.log(response.data);
-                    this.answer = JSON.parse(response.data);
-                }
-                console.log(response.message);
-                this.server_message = response.message;
-                this.server_message_type = response.message_type;
-                this.server_message_handle.$emit('animate');
+            triggerReset(affirm){
+                this.$emit('reset', affirm);
             }
         },
-        mounted() {
-            $(window).bind('keydown', this.keyEvent);
-        },
+        watch: {
+            dataAnswer: function(val){
+                this.answer = this.getAnswerConversion(val);
+            }
+        }
     }
 </script>
 
